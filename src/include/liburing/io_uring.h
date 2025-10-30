fde1ee69cb7c052a4e130a7b295991d66d113f03
Pavel Begunkov <asml.silence@gmail.com>
2025-10-30 11:35:52 +0000

    Update io_uring.h
    Pull in zcrx sharing uapi
    
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- src/include/liburing/io_uring.h
+++ src/include/liburing/io_uring.h
@@ -635,6 +635,11 @@ enum io_uring_register_op {
 
 	IORING_REGISTER_MEM_REGION		= 34,
 
+	/* query various aspects of io_uring, see linux/io_uring/query.h */
+	IORING_REGISTER_QUERY			= 35,
+
+	IORING_REGISTER_ZCRX_CTRL		= 36,
+
 	/* this goes last */
 	IORING_REGISTER_LAST,
 
@@ -986,6 +991,19 @@ struct io_uring_zcrx_ifq_reg {
 	__u32	zcrx_id;
 	__u32	__resv2;
 	__u64	__resv[3];
+};
+
+enum zcrx_ctrl_op {
+	ZCRX_CTRL_EXPORT,
+	ZCRX_CTRL_IMPORT,
+
+	__ZCRX_CTRL_LAST,
+};
+
+struct zcrx_ctrl {
+	__u32	zcrx_id;
+	__u32	op; /* see enum zcrx_ctrl_op */
+	__u64	resv[8];
 };
 
 #ifdef __cplusplus
