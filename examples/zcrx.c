5d8c767e2477615785dc196b054c2938812256f7
Pavel Begunkov <asml.silence@gmail.com>
2026-02-01 13:50:05 +0000

    examples/zcrx: parameterise with rx buf len
    Signed-off-by: Pavel Begunkov <asml.silence@gmail.com>

--- examples/zcrx.c
+++ examples/zcrx.c
@@ -102,6 +102,7 @@ static size_t cfg_size = 0;
 static unsigned cfg_affinity_mode = AFFINITY_MODE_NONE;
 static unsigned cfg_rq_alloc_mode = RQ_ALLOC_USER;
 static unsigned cfg_area_type = AREA_TYPE_NORMAL;
+static unsigned cfg_rx_buf_len;
 
 static long page_size;
 
@@ -292,6 +293,7 @@ static void setup_zcrx(struct io_uring *ring)
 		.rq_entries = rq_entries,
 		.area_ptr = uring_ptr_to_u64(&area_reg),
 		.region_ptr = uring_ptr_to_u64(&region_reg),
+		.rx_buf_len = cfg_rx_buf_len,
 	};
 
 	ret = io_uring_register_ifq(ring, &reg);
@@ -614,7 +616,7 @@ static void parse_opts(int argc, char **argv)
 	if (argc <= 1)
 		usage(argv[0]);
 
-	while ((c = getopt(argc, argv, "vp:i:q:s:r:A:S:C:R:c:")) != -1) {
+	while ((c = getopt(argc, argv, "vp:i:q:s:r:A:S:C:R:c:B:")) != -1) {
 		switch (c) {
 		case 'p':
 			cfg_port = strtoul(optarg, NULL, 0);
@@ -654,6 +656,9 @@ static void parse_opts(int argc, char **argv)
 			cfg_affinity_mode = strtoul(optarg, NULL, 0);
 			if (cfg_affinity_mode >= __AFFINITY_MODE_MAX)
 				t_error(1, 0, "Invalid affinity mode");
+			break;
+		case 'B':
+			cfg_rx_buf_len = strtoul(optarg, NULL, 0);
 			break;
 		}
 	}
